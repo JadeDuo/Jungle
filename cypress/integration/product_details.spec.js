c41f3ad669f4534239af437f7b63c0b785f472e3
Jade <jade.duong@telus.com>
2023-01-31 12:25:36 -0800

    add product page tests

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,4 +1,4 @@
-describe('Loads Jungle and displays products', () => {
+describe('Loads Jungle ', () => {
   it('should visit', () => {
     cy.visit('/')
   })
@@ -7,5 +7,11 @@ describe('Loads Jungle and displays products', () => {
   });
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
+  });
+
+  it("should click on a product and load that page", () => {
+    cy.contains('Scented Blade').click()
+
+    cy.get(".product-detail").should("be.visible");
   });
 })
